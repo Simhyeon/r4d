d8fb97c391afd44376a5b083ce006ef564dcae2e
simoncreek <simoncreek@tutanota.com>
2021-08-10 03:20:01 +0900

    Added several errors

---
+++ src/error.rs
@@ -0,0 +1,39 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum CliError{
+    #[error("Unkown option was given")]
+    Unknown,
+}
+
+#[derive(Error, Debug)]
+pub enum BasicError{
+    #[error("Invalid regex statement : {0}")]
+    InvalidRegex(regex::Error),
+    #[error("Invalid formula : {0}")]
+    InvalidFormula(evalexpr::EvalexprError)
+}
+
+impl From<regex::Error> for BasicError {
+    fn from(err : regex::Error) -> Self {
+        Self::InvalidRegex(err)
+    }
+}
+
+impl From<evalexpr::EvalexprError> for BasicError {
+    fn from(err : evalexpr::EvalexprError) -> Self {
+        Self::InvalidFormula(err)
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum MainError{
+    #[error("Command line error of : {0}")]
+    Cli(CliError),
+}
+
+impl From<CliError> for MainError {
+    fn from(err : CliError) -> Self {
+        Self::Cli(err)
+    }
+}
