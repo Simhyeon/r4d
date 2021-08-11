9358ccc87baf3386f55704e6471c22296afdf688
simoncreek <simoncreek@tutanota.com>
2021-08-11 21:10:23 +0900

    New erros

--- src/error.rs
+++ src/error.rs
@@ -12,6 +12,10 @@ pub enum BasicError{
     InvalidRegex(regex::Error),
     #[error("Invalid formula : {0}")]
     InvalidFormula(evalexpr::EvalexprError),
+    #[error("Invalid argument : {0}")]
+    InvalidArgument(&'static str),
+    #[error("Invalid argument type: {0}")]
+    InvalidArgType(std::num::ParseIntError)
 }
 
 impl From<regex::Error> for BasicError {
@@ -23,6 +27,12 @@ impl From<regex::Error> for BasicError {
 impl From<evalexpr::EvalexprError> for BasicError {
     fn from(err : evalexpr::EvalexprError) -> Self {
         Self::InvalidFormula(err)
+    }
+}
+
+impl From<std::num::ParseIntError> for BasicError {
+    fn from(err : std::num::ParseIntError) -> Self {
+        Self::InvalidArgType(err)
     }
 }
 
