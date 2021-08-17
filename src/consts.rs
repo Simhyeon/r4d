f1b367b995d600ff73bc86229a534912539c7cd8
simoncreek <simoncreek@tutanota.com>
2021-08-17 16:46:26 +0900

    Line ending constant

--- src/consts.rs
+++ src/consts.rs
@@ -1,3 +1,8 @@
 pub const MACRO_START_CHAR: char = '$';
 pub const ESCAPE_CHAR: char ='\\';
 pub const MAIN_CALLER: &str = "@MAIN";
+
+#[cfg(windows)]
+pub const LINE_ENDING: &'static str = "\r\n";
+#[cfg(not(windows))]
+pub const LINE_ENDING: &'static str = "\n";
