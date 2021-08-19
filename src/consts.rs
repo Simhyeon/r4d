c521aef9c6bae80dfd75996c5e4743c57d17013f
simoncreek <simoncreek@tutanota.com>
2021-08-19 23:58:11 +0900

    New comma rule

--- src/consts.rs
+++ src/consts.rs
@@ -1,6 +1,7 @@
 pub const MACRO_START_CHAR: char = '$';
 pub const ESCAPE_CHAR: char ='\\';
 pub const MAIN_CALLER: &str = "@MAIN@";
+pub const ESCAPED_COMMA : &str = "@COMMA@";
 
 #[cfg(windows)]
 pub const LINE_ENDING: &'static str = "\r\n";
