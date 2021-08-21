f31bdef94d65c56dd1eff8730148d98f7015ccf1
simoncreek <simoncreek@tutanota.com>
2021-08-22 03:30:24 +0900

    Added temp logics

--- src/consts.rs
+++ src/consts.rs
@@ -7,3 +7,10 @@ pub const ESCAPED_COMMA : &str = "@COMMA@";
 pub const LINE_ENDING: &'static str = "\r\n";
 #[cfg(not(windows))]
 pub const LINE_ENDING: &'static str = "\n";
+
+#[cfg(windows)]
+pub const TEMP_PATH: &'static str = "%TEMP%";
+#[cfg(not(windows))]
+pub const TEMP_PATH: &'static str = "/tmp";
+
+pub const TEMP_FILE: &'static str = "rad_temp.txt";
