f7595c9007ae84f129081e6ed7c88eb253f9bd60
simoncreek <simoncreek@tutanota.com>
2021-09-16 04:18:39 +0900

    Added placeholder for debuggers

---
+++ src/debugger.rs
@@ -0,0 +1,22 @@
+pub struct Debugger;
+
+impl Debugger {
+    pub fn show_logs() {
+
+    }
+    pub fn set_break_point() {
+
+    }
+    
+    pub fn goto_next_break_point() {
+
+    }
+
+    pub fn next_line() {
+
+    }
+
+    pub fn next_macro() {
+
+    }
+}
