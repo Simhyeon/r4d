c6fd0914f3770415d0d322a6df53da5a621a53d0
simoncreek <simoncreek@tutanota.com>
2021-09-26 16:35:30 +0900

    Added code for less compatibility

--- src/bin.rs
+++ src/bin.rs
@@ -4,6 +4,11 @@ use rad::RadError;
 
 pub fn main() -> Result<(), RadError> {
 
+    // Enable color on pager such as "less" 
+    // by overloading color related environment
+    #[cfg(feature = "color")]
+    colored::control::set_override(true);
+
     // Command line parse
     #[cfg(feature = "clap")]
     if let Err(content) = Cli::new().parse() {
