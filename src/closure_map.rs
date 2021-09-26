f5f6cdd6d2d911d2daa800e1d9a895ff6d726e9a
simoncreek <simoncreek@tutanota.com>
2021-09-26 21:10:56 +0900

    Added new closure related module

---
+++ src/closure_map.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+use crate::error::RadError;
+use crate::arg_parser::ArgParser;
+
+pub(crate) struct ClosureMap {
+    map: HashMap<String, Box<dyn FnMut(&str,bool) -> Result<Option<String>, RadError>>>,
+}
+
+impl ClosureMap {
+    /// Create new closure map
+    pub fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
+    }
+    /// Check if map contains the name
+    pub fn contains(&self, name: &str) -> bool {
+        self.map.contains_key(name)
+    }
+
+    /// Add new closure
+    pub fn add_new(&mut self, name: &'static str, arg_count: usize, mut closure : Box<dyn FnMut(Vec<String>) -> Option<String>>) 
+    {
+        self.map.insert(
+            name.to_owned(), 
+            Box::new(move |args: &str, greedy:bool| -> Result<Option<String>, RadError> {  
+                if let Some(args) = ArgParser::new().args_with_len(args, arg_count, greedy) {
+                    Ok(closure(args))
+                } else {
+                    Err(RadError::InvalidArgument(format!("Argument of \"{}\" is not sufficient.",name)))
+                }
+            })
+        );
+    }
+
+    /// Execute closure by name
+    pub fn call(&mut self, name: &str, args: &str, greedy: bool) -> Result<Option<String>, RadError> {
+        if let Some(closure) = self.map.get_mut(name) {
+            Ok(closure(args,greedy)?)
+        } else {
+            Ok(None)
+        }
+    }
+}
