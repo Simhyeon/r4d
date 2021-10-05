4879436f34bcd403b9d28d3303644bc342b77aee
simoncreek <simoncreek@tutanota.com>
2021-10-06 00:11:59 +0900

    Bumped version

--- src/cli.rs
+++ src/cli.rs
@@ -161,7 +161,7 @@ impl Cli {
     /// Creates argument template wich clap macro
     fn args_builder() -> clap::ArgMatches {
         clap_app!(rad =>
-            (version: "0.10.0")
+            (version: "0.10.1")
             (author: "Simon Creek <simoncreek@tutanota.com>")
             (about: "R4d is a modern macro processor made with rust")
             (@arg FILE: ... "Files to execute processing")
