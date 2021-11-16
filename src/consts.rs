cbfde48bf9506bf7d4105ef4756c6e17aade01ed
simoncreek <simoncreek@tutanota.com>
2021-11-17 01:31:43 +0900

    Made comment and macro rules to be configurable

--- src/consts.rs
+++ src/consts.rs
@@ -1,20 +1,48 @@
+use regex::Regex;
+
+// Get macro start character
+pub(crate) fn macro_start(custom : Option<char>) -> char {
+    if let Some(start) = custom{
+        start
+    } else {
+        MACRO_START_CHAR
+    }
+}
+
+// Get comment start chracter
+pub(crate) fn comment_start(custom: Option<char>) -> char {
+    if let Some(start) = custom{
+        start
+    } else {
+        COMMENT_CHAR
+    }
+}
+
 // Platform agonistic consts
-pub const MACRO_START_CHAR: char = '$';
+const MACRO_START_CHAR: char = '$';
+const COMMENT_CHAR : char = '%';
 pub const ESCAPE_CHAR: char ='\\';
+pub const LIT_CHAR: char = '*';
 pub const MAIN_CALLER: &str = "@MAIN@";
+
+lazy_static::lazy_static! {
+    // Numbers
+    // Macro attributes * ^ | +
+    // Underscore and reverse slash (\)
+    pub static ref UNALLOWED_CHARS: Regex = Regex::new(r#"[a-zA-Z1-9\\_\*\^\|\+\(\)=,]"#).expect("Failed to create regex expression");
+}
+
+// Diff related
 #[cfg(feature = "debug")]
 pub const DIFF_SOURCE_FILE : &str = "diff.src";
 #[cfg(feature = "debug")]
 pub const DIFF_OUT_FILE : &str = "diff.out";
-pub const COMMENT_CHAR : char = '%';
 
 // Platform specific consts
-
 #[cfg(windows)]
 pub const LINE_ENDING: &'static str = "\r\n";
 #[cfg(not(windows))]
 pub const LINE_ENDING: &'static str = "\n";
-pub const LIT_CHAR: char = '*';
 
 // Option specific consts
 #[cfg(feature = "evalexpr")]
