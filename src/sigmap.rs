c1f5621cc7510c8ec8ee1f8e43f042121b82a59a
simoncreek <simoncreek@tutanota.com>
2021-11-30 00:14:21 +0900

    New sigmap mod

---
+++ src/sigmap.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+use std::iter::FromIterator;
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SignatureMap {
+    pub object : HashMap<String, MacroSignature>,
+}
+
+impl SignatureMap {
+    pub fn new(sigs: Vec<MacroSignature>) -> Self {
+        let sig = HashMap::from_iter(
+            sigs.into_iter().map(|sig| (sig.name.clone(), sig))
+        );
+        Self { object: sig }
+    }
+}
+
+/// Type(variant) of macro
+#[derive(Debug, Serialize, Deserialize)]
+pub enum MacroVariant {
+    Keyword,
+    Basic,
+    Custom,
+}
+
+/// Macro signature
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MacroSignature {
+    pub variant: MacroVariant,
+    pub name: String,
+    pub args: Vec<String>,
+    pub expr: String,
+}
