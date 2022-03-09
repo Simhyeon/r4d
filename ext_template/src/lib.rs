346e5308417ed5861fddff94939f044ddf279162
simoncreek <simoncreek@tutanota.com>
2022-03-10 01:40:27 +0900

    New ext template macro

---
+++ ext_template/src/lib.rs
@@ -0,0 +1,25 @@
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn function_template(item: TokenStream) -> TokenStream {
+    format!("|args : &str, processor : &mut Processor| -> RadResult<Option<String>> {{ 
+        {}
+}}",item).parse().unwrap()
+}
+
+#[proc_macro]
+pub fn keyword_template(item: TokenStream) -> TokenStream {
+    format!("|args : &str, level: usize, processor : &mut Processor| -> RadResult<Option<String>> {{ 
+        {}
+}}",item).parse().unwrap()
+}
+
+#[proc_macro]
+pub fn expand(item: TokenStream) -> TokenStream {
+    format!("processor.expand(level,{})", item).parse().unwrap()
+}
+
+#[proc_macro]
+pub fn split_args(item: TokenStream) -> TokenStream {
+    format!("processor.get_split_arguments({}, args)", item).parse().unwrap()
+}
