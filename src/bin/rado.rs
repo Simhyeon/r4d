b8d4cc7a1387e6488657c1292ad9cdf407250777
simoncreek <simoncreek@tutanota.com>
2022-06-12 05:40:42 +0900

    Relocated bin to separate directory

---
+++ src/bin/rado.rs
@@ -0,0 +1,19 @@
+use rad::RadResult;
+
+pub fn main() -> RadResult<()> {
+    // Enable color on pager such as "less"
+    // by overloading color related environment
+    #[cfg(feature = "color")]
+    colored::control::set_override(true);
+
+    // Command line parse
+    #[cfg(feature = "clap")]
+    {
+        use rad::RadoCli;
+        use std::io::Write;
+        if let Err(content) = RadoCli::new().parse() {
+            writeln!(std::io::stderr(), "{}", content)?;
+        }
+    }
+    Ok(())
+}
