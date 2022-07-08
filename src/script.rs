f060967c8e2a2c405ff12af92867ae181bcf200e
simoncreek <simoncreek@tutanota.com>
2022-07-09 00:23:38 +0900

    New default script file

--- src/script.rs
+++ src/script.rs
@@ -1,5 +1,10 @@
+// NOTE
+// Leave imports as it is if you are not sure about it
+
 #[allow(unused_imports)]
 use crate::rad_ext_template::*;
+#[allow(unused_imports)]
+use crate::AuthType;
 #[allow(unused_imports)]
 use crate::ExtMacroBuilder;
 use crate::Processor;
@@ -14,33 +19,61 @@ pub fn extend_processor(processor: &mut Processor) -> RadResult<()> {
     // ---
     // Write your custom extension macros from here
 
+    // NOTE
+    // Remove surrounding quote /* */ to uncomment
+
     /*
-    // DEMOS
     // Extend function macro
     processor.add_ext_macro(
-        ExtMacroBuilder::new("macro_name")
-            .args(&["a1", "b2"])
+        ExtMacroBuilder::new("mac1")
+            .args(&["a_first", "a_second"])
+            .desc("Description comes here")
             .function(function_template!(
-                    let args = split_args!(2)?;
+                    // NOTE
+                    // Function macro does not need to expand arguments
+                    // Because it is already expanded
+
+                    // NOTE
+                    // On split_args macro,
+                    // Function macro should give second argument as "false"
+                    // If true, then it will not strip literal quotes
+                    let args = split_args!(2,false)?;
                     let result = format!("{} + {}", args[0], args[1]);
                     Ok(Some(result))
             )),
     );
     // Extend deterred macro
     processor.add_ext_macro(
-        ExtMacroBuilder::new("macro_name")
-            .args(&["a1", "b2"])
+        ExtMacroBuilder::new("wow")
+            .args(&["a_first", "a_second"])
+            .desc("Description comes here")
             .deterred(deterred_template!(
-                    audit_auth!("macro_name",crate::AuthType::CMD);
-                    let args = split_args!(2)?;
-                    let result = if expand!(&args[0])? == "doit" {
-                        Some(format!("I did it -> {}", expand!(&args[1])?))
+                    // NOTE
+                    // Audit authentication
+                    // Macro name, and required auth type
+                    audit_auth!("macro_name",AuthType::CMD);
+
+                    // NOTE
+                    // On split_args macro,
+                    // Deterred macro should have second argument as "true"
+                    // because deterred should not strip before expansion
+                    let args = split_args!(2,true)?;
+
+                    // NOTE
+                    // expand_args macro should be only called on deterred macros
+                    // because it expands arguments and also strip
+                    let result = if expand_args!(&args[0])? == "doit" {
+
+                        // NOTE
+                        // You can expand normal expression with expand_args macro
+                        Some(format!("I did it -> {}", expand_args!(&args[1])?))
                     } else { None };
                     Ok(result)
             )),
     );
     */
 
+    // NOTE
     // Should return Ok(()) at the end
     // ---
     Ok(())
