6a611de51ff25500afe35a90515d4ca74c8f97e7
simoncreek <simoncreek@tutanota.com>
2024-01-28 17:03:27 +0900

    Test update

--- src/test.rs
+++ src/test.rs
@@ -1,29 +1,5 @@
 use crate::utils::Utils;
-use crate::{NewArgParser, RadError, RadResult, RadStorage, StorageOutput, StorageResult};
-use once_cell::sync::Lazy;
-use regex::Regex;
-use std::borrow::Cow;
-use std::io::Write;
-
-pub struct TestStorage;
-
-/// Regex for leading and following spaces
-static LF_SPACES: Lazy<Regex> = Lazy::new(|| Regex::new(r"(^[ \t\r\n]+)\S*([ \t\r\n]+$)").unwrap());
-static LSPA: Lazy<Regex> = Lazy::new(|| Regex::new(r"(^[ \t\r\n]+)").unwrap());
-static FSPA: Lazy<Regex> = Lazy::new(|| Regex::new(r"([ \t\r\n]+$)").unwrap());
-
-// impl RadStorage for TestStorage {
-//     fn update(&mut self, args: &[String]) -> crate::StorageResult<()> {
-//         match args[0].as_str() {
-//             "err" => return StorageResult::Err(Box::new(RadError::Interrupt)),
-//             _ => return StorageResult::Ok(()),
-//         }
-//     }
-//
-//     fn extract(&mut self, serialize: bool) -> crate::StorageResult<Option<crate::StorageOutput>> {
-//         StorageResult::Ok(None)
-//     }
-// }
+use crate::NewArgParser;
 
 #[test]
 fn arg_test() {
